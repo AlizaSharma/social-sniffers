efcf894c949114db3f682f5115fe0b7da1ee9da7
Aliza Sharma <aliza.sharma12@gmail.com>
2023-09-07 15:54:27 -0500

    initial file setup

---
+++ controllers /homeRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+})
+
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+router.get('/profile', (res, req) => {
+    res.render('profile');
+})
+
+
+
+
+
+
+
+module.exports = router;
