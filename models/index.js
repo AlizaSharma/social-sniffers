b440efd93b6766021101e2eeaf099ee8ff328575
Mona Lisa <cordovamonica.a@gmail.com>
2023-09-10 11:12:44 -0500

    fixed index in models, added to login view file

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Post = require('./Post');
+const Forum = require('./Forum');
 
-User.hasMany(Post, {
+User.hasMany(Forum, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Post.belongsTo(User, {
+Forum.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+module.exports = { User, Forum };
